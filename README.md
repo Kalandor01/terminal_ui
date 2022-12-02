3a45af24491dedacb6fb98eb5f3315bc304af947
Kalandor01 <rohovszkyakoska@gmail.com>
2022-12-02 23:40:12 +0100

    N

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This module has functios for a displaying basic UI elements using the terminal, with the screen_display module.
+This module has functions for a displaying basic UI elements using the terminal, with the screen_display module.
--- terminal_ui/__init__.py
+++ terminal_ui/__init__.py
@@ -1,5 +1,5 @@
 """
-This module has functios for a displaying basic UI elements using the terminal, with the `screen_display` module.
+This module has functions for a displaying basic UI elements using the terminal, with the `screen_display` module.
 """
 __version__ = "0.1"
 
